66e1c11a01c48ac9d21b7ce54c4b59d4b7d32aeb
Tijmen <tijmen.vaneijk@gmail.com>
2017-05-12 08:51:38 +0200

    Changed 1 bit to go to 4 MB in Cache.vhd

--- plasma_PDP/rtl/cache.vhd
+++ plasma_PDP/rtl/cache.vhd
@@ -99,7 +99,7 @@ begin
 
       if state = STATE_IDLE then    --check if next access in cached range
          cache_address <= address_next(12 downto 2); -- change: to 12 concatenation with 0 removed
-         if address_next(30 downto 21) = "0010000000" then  --first 2MB of DDR
+         if address_next(30 downto 21) = "0100000000" then  --first 2MB of DDR -- TvE: changed from 0010000000 to 0100000000
             cache_access <= '1';
             if byte_we_next = "0000" then     --read cycle
                cache_we <= '0';
